0c11d6e40c9d40b3e41513bd2085c5ba515b255f
Marcus Carter <mac987@gmail.com>
2024-05-11 02:03:40 +0000

    add ping

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@
 - Route Organization
 '''
 
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, __version__
 from routes.players import playerrouter
 from routes.courses import courserouter
 from routes.rounds import roundrouter
-from fastui import AnyComponent, FastUI
-from fastui import components as c
+from time import time
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI(title='POSTscore',
@@ -29,68 +30,38 @@ app = FastAPI(title='POSTscore',
         }
     ]
 )
-
-
-'''
-# Configure CORS
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://10.0.1.45:*"
-    # Add more origins as needed
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-'''
 app.include_router(playerrouter)
 app.include_router(courserouter)
 app.include_router(roundrouter)
 
 router = APIRouter()
 
-@router.get('/', response_model=FastUI, response_model_exclude_none=True)
-def api_index() -> list[AnyComponent]:
-    # language=markdown
-    markdown = """\
-This site provides a demo of [FastUI](https://github.com/pydantic/FastUI), the code for the demo
-is [here](https://github.com/pydantic/FastUI/tree/main/demo).
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-The following components are demonstrated:
-
-* `Markdown` — that's me :-)
-* `Text`— example [here](/components#text)
-* `Paragraph` — example [here](/components#paragraph)
-* `PageTitle` — you'll see the title in the browser tab change when you navigate through the site
-* `Heading` — example [here](/components#heading)
-* `Code` — example [here](/components#code)
-* `Button` — example [here](/components#button-and-modal)
-* `Link` — example [here](/components#link-list)
-* `LinkList` — example [here](/components#link-list)
-* `Navbar` — see the top of this page
-* `Footer` — see the bottom of this page
-* `Modal` — static example [here](/components#button-and-modal), dynamic content example [here](/components#dynamic-modal)
-* `ServerLoad` — see [dynamic modal example](/components#dynamic-modal) and [SSE example](/components#server-load-sse)
-* `Image` - example [here](/components#image)
-* `Iframe` - example [here](/components#iframe)
-* `Video` - example [here](/components#video)
-* `Table` — See [cities table](/table/cities) and [users table](/table/users)
-* `Pagination` — See the bottom of the [cities table](/table/cities)
-* `ModelForm` — See [forms](/forms/login)
-
-Authentication is supported via:
-* token based authentication — see [here](/auth/login/password) for an example of password authentication
-* GitHub OAuth — see [here](/auth/login/github) for an example of GitHub OAuth login
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
 """
 
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 
-@router.get('/{path:path}', status_code=404)
-async def api_404():
-    # so we don't fall through to the index page
-    return {'message': 'Not Found'}
-
+@app.get('/ping')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
